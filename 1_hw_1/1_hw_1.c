27db47d4086ad95d85d81ed6dcf0d8d5cdb54d53
gsval <115543494+gsval@users.noreply.github.com>
2022-10-27 19:32:10 +0300

    исправлена ошибка в первом задании

--- 1_hw_1/1_hw_1.c
+++ 1_hw_1/1_hw_1.c
@@ -4,20 +4,20 @@
 #include<stdlib.h>
 int main() {
 	setlocale(LC_ALL, "russian");
-	float x1, y1, x2, y2, x3, y3;
+	int x1, y1, x2, y2, x3, y3;
 
-	printf("Ведите координаты вершин треугольника:");
+	printf("Г‚ГҐГ¤ГЁГІГҐ ГЄГ®Г®Г°Г¤ГЁГ­Г ГІГ» ГўГҐГ°ГёГЁГ­ ГІГ°ГҐГіГЈГ®Г«ГјГ­ГЁГЄГ :");
 	scanf_s("%d %d %d %d %d %d", &x1, &y1, &x2, &y2, &x3, &y3);
 
 	//int S = abs((x2 - x1) * (y3 - y1) - (x3 - x1) * (y2 - y1)) / 2
-	float a = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
-	float b = sqrt(pow(x3 - x2, 2) + pow(y3 - y2, 2));
-	float c = sqrt(pow(x1 - x3, 2) + pow(y1 - y3, 2));
-	float P = a + b + c;
-	float p = P / 2;
-	float S = sqrt(p * (p - a) * (p - b) * (p - c));
-	printf("P треугольника: P= %0.2d\n", P);
-	printf("S треугольника: S= %0.2d\n", S);
+	int a = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+	int b = sqrt(pow(x3 - x2, 2) + pow(y3 - y2, 2));
+	int c = sqrt(pow(x1 - x3, 2) + pow(y1 - y3, 2));
+	int P = a + b + c;
+	int p = P / 2;
+	int S = sqrt(p * (p - a) * (p - b) * (p - c));
+	printf("P ГІГ°ГҐГіГЈГ®Г«ГјГ­ГЁГЄГ : P= %0.2d\n", P);
+	printf("S ГІГ°ГҐГіГЈГ®Г«ГјГ­ГЁГЄГ : S= %0.2d\n", S);
 	system("pause");
 	return 0;
 }
